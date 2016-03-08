91dbffc2fda942630b1eee6b2fc41d57b10dd538
inakidml <inaki.diaz@ikasle.egibide.org>
2016-03-08 15:20:05 +0100

    available

--- 09_alarma/EjercicioAlarma/src/ejercicioalarma/EjercicioAlarma.java
+++ 09_alarma/EjercicioAlarma/src/ejercicioalarma/EjercicioAlarma.java
@@ -26,16 +26,25 @@ public class EjercicioAlarma {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int opcion;
+        Boolean mostrarMenu=true;
 
         do {
-            System.out.println("1. Activar alarma\n"
+            opcion=0;
+            if (mostrarMenu) {
+                System.out.println("1. Activar alarma\n"
                     + "2. Desactivar alarma (necesario PIN)\n"
                     + "3. Consultar sensores\n"
                     + "4. Salir");
             System.out.print("Opción: ");
-            opcion = Integer.parseInt(br.readLine());
-
+            mostrarMenu=false;
+            }
+            
+            if (System.in.available()>0) {//si hay dato lee
+             opcion = Integer.parseInt(br.readLine());
+            }
             switch (opcion) {
+                case 0:
+                    break;
                 case 1:
                     // Pedir el teléfono de aviso
                     System.out.print("Escribe el teléfono de aviso: ");
@@ -45,21 +54,25 @@ public class EjercicioAlarma {
 
                     // Activar la alarma
                     alarma.activar();
+                    mostrarMenu=true;
                     break;
                 case 2:
                     System.out.print("Escribe el PIN: ");
                     int PIN = Integer.parseInt(br.readLine());
 
                     alarma.desactivar(PIN);
+                    mostrarMenu=true;
                     break;
                 case 3:
                     alarma.consultarSensores();
+                    mostrarMenu=true;
                     break;
                 case 4:
                     break;
                 default:
                     System.out.println("ERROR: Opción incorrecta...");
             }
+            
         } while (opcion != 4);
 
     }
